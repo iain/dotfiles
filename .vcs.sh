02879550ec56d0b3cf8c04ece3520b798fcb3251
iain <iain@iain.nl>
2009-09-11 05:23:08 -0700

    Remove dirty state when moving out of project
    Also removed some useless bulk

--- .vcs.sh
+++ .vcs.sh
@@ -1,42 +1,3 @@
-# misc
-NO_COLOR='\e[0m' #disable any colors
-# regular colors
-BLACK='\e[0;30m'
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-YELLOW='\e[0;33m'
-BLUE='\e[0;34m'
-MAGENTA='\e[0;35m'
-CYAN='\e[0;36m'
-WHITE='\e[0;37m'
-# emphasized (bolded) colors
-EBLACK='\e[1;30m'
-ERED='\e[1;31m'
-EGREEN='\e[1;32m'
-EYELLOW='\e[1;33m'
-EBLUE='\e[1;34m'
-EMAGENTA='\e[1;35m'
-ECYAN='\e[1;36m'
-EWHITE='\e[1;37m'
-# underlined colors
-UBLACK='\e[4;30m'
-URED='\e[4;31m'
-UGREEN='\e[4;32m'
-UYELLOW='\e[4;33m'
-UBLUE='\e[4;34m'
-UMAGENTA='\e[4;35m'
-UCYAN='\e[4;36m'
-UWHITE='\e[4;37m'
-# background colors
-BBLACK='\e[40m'
-BRED='\e[41m'
-BGREEN='\e[42m'
-BYELLOW='\e[43m'
-BBLUE='\e[44m'
-BMAGENTA='\e[45m'
-BCYAN='\e[46m'
-BWHITE='\e[47m'
-
 __prompt_command() {
   local vcs base_dir sub_dir ref last_command svnversion
   sub_dir() {
@@ -139,6 +100,7 @@ __prompt_command() {
     __vcs_sub_dir="${sub_dir}"
     __vcs_base_dir="${base_dir/$HOME/~}"
   else
+    __dirty=''
     __vcs_prefix=''
     __vcs_base_dir="${PWD/$HOME/~}"
     __vcs_ref=''
@@ -154,14 +116,3 @@ __prompt_command() {
 PROMPT_COMMAND=__prompt_command
 
 PS1='\[\033[1;32;34m\]${__vcs_base_dir}\[\033[1;32;31m\]${__dirty}\[\033[1;32;33m\]${__vcs_ref}\[\033[1;32;34m\]${__vcs_sub_dir}\[\033[1;32;32m\] \$\[\033[00;00m\] '
-  #PS1='\[\e]2;\h:$__pretty_pwd\a\e]1;$__tab_title\a\]\[\033[1;32;34m\]${__vcs_base_dir}${__dirty}\[\033[1;32;33m\]${__vcs_ref}\[\033[1;32;34m\]${__vcs_sub_dir}\[\033[1;32;32m\] \$\[\033[00;00m\] '
-
-# Show the currently running command in the terminal title:
-# http://www.davidpashley.com/articles/xterm-titles-with-bash.html
-#if [ -z "$TM_SUPPORT_PATH"]; then
-#case $TERM in
-#  rxvt|*term|xterm*)
-#    trap 'echo -e "\e]2;$working_on$BASH_COMMAND\007\c"' DEBUG
-#  ;;
-#esac
-#fi
