5ebc12965a27840f69f9870c0a5cc8f73172cf43
Iain Hecker <iain@iain.nl>
2011-02-06 12:43:09 +0100

    More advanced titles; fix rvm opening in new tab

--- .zshrc
+++ .zshrc
@@ -1,4 +1,4 @@
 PROJECT_PARENT_DIRS=()
-PROJECT_PARENT_DIRS+=("$HOME/Projects")
+PROJECT_PARENT_DIRS+=("$HOME/Code")
 
-source .osx_settings/zsh/rc
+source ~/.osx_settings/zsh/rc
--- zsh/12_title.zsh
+++ zsh/12_title.zsh
@@ -1,16 +1,39 @@
 case $TERM in (*xterm*|ansi)
+
+  # set the terminal title and tab title when switching directories
+  function settab { print -Pn "\e]1;%n: %~\a" }
+  function settitle { print -Pn "\e]2;%n: %~\a" }
+  function chpwd { settab; settitle }
+  settab; settitle
+
+  # For new tabs don't show the previews command from history,
+  # it has nothing to do with you just yet.
+  NEWLY_OPENED="yes"
+
+  # sets the tab title to the currently running command (not process)
   preexec() {
-    print -Pn "\e]0;Running: $1\a"
+    NEWLY_OPENED=""
+    print -Pn "\e]1;Running: $1\a"
+    # settitle
   }
+
+  # sets the tab title to the result of the last run command (not process)
+  # when last command has failed, also set the title because it's important
   precmd() {
     success=$?
-    cmd=`history | tail -n 1 | sed 's/[^0-9]*[0-9]*\ \ //'`
-    if [[ $success != 0 ]]; then
-      print -Pn "\e]0;Error: $cmd\a"
+    if [ -z $NEWLY_OPENED ]; then
+      cmd=`history | tail -n 1 | sed 's/[^0-9]*[0-9]*\ \ //'`
+      if [[ $success != 0 ]]; then
+        print -Pn "\e]0;Error: $cmd\a"
+      else
+        print -Pn "\e]1;Finished: $cmd\a"
+        settitle
+      fi
     else
-      print -Pn "\e]0;Finished: $cmd\a"
+      # When using iTerm2's "Reuse Previous Tab Directory", it doesn't load the right ruby/gemset
+      # This is the safest and easiest way I could think about to "fix" it
+      rvm reload
     fi
   }
   ;;
 esac
-
