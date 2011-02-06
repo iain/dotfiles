e223b44029f2b615e09530f1ca03076e00919d3e
Iain Hecker <iain@iain.nl>
2011-02-06 03:08:36 +0100

    Spaces people

--- .project_aliases.sh
+++ .project_aliases.sh
@@ -4,18 +4,18 @@
 #  source ~/.project_aliases.sh
 
 if [ -z "${PROJECT_PARENT_DIRS[*]}" ]; then
-	echo "Define a PROJECT_PARENT_DIRS array in ~/.bash_profile"
+  echo "Define a PROJECT_PARENT_DIRS array in ~/.bash_profile"
 fi
 
 for PARENT_DIR in ${PROJECT_PARENT_DIRS[@]} ; do
   if [ -d "$PARENT_DIR" ]; then
     for PROJECT_DIR in $(/bin/ls $PARENT_DIR); do
-			if [ ! -z `which $PROJECT_DIR` ]; then
+      if [ ! -z `which $PROJECT_DIR` ]; then
         continue # don't set alias if there is something already a command on the path with the same name
       fi
-			if [ -d "$PARENT_DIR/$PROJECT_DIR" ]; then
-	      alias "$PROJECT_DIR"="cd $PARENT_DIR/$PROJECT_DIR"
-			fi
+      if [ -d "$PARENT_DIR/$PROJECT_DIR" ]; then
+        alias "$PROJECT_DIR"="cd $PARENT_DIR/$PROJECT_DIR"
+      fi
     done
   fi
 done
