2201695fadd58d78457ad98c100d525381b471bb
iain <iain@iain.nl>
2011-09-03 19:08:51 +0200

    Fix bug in project parent dirs and don't output anymore

--- bash/99_project_aliases.sh
+++ bash/99_project_aliases.sh
@@ -3,10 +3,6 @@
 #  PROJECT_PARENT_DIRS[1]="$HOME/work"
 #  source ~/.project_aliases.sh
 
-if [ -z "${PROJECT_PARENT_DIRS[*]}" ]; then
-  echo "Define a PROJECT_PARENT_DIRS array in ~/.bash_profile"
-fi
-
 for PARENT_DIR in ${PROJECT_PARENT_DIRS[@]} ; do
   if [ -d "$PARENT_DIR" ]; then
     for PROJECT_DIR in $(/bin/ls $PARENT_DIR); do
--- zsh/99_project_aliases.zsh
+++ zsh/99_project_aliases.zsh
@@ -2,15 +2,10 @@
 # PROJECT_PARENT_DIRS=()
 # PROJECT_PARENT_DIRS+=("$HOME/Projects")
 
-if [ -z "${PROJECT_PARENT_DIRS[*]}" ]; then
-  echo "Define a PROJECT_PARENT_DIRS array in ~/.bash_profile"
-fi
-
 for PARENT_DIR in ${PROJECT_PARENT_DIRS[@]} ; do
   if [ -d "$PARENT_DIR" ]; then
     for PROJECT_DIR in $(/bin/ls $PARENT_DIR); do
-      found=`wich $PROJECT_DIR 2> /dev/null`
-      if [ -z $found ]; then
+      if [[ ! -z `which $PROJECT_DIR 2> /dev/null` ]]; then
         if [ -d "$PARENT_DIR/$PROJECT_DIR" ]; then
           alias "$PROJECT_DIR"="cd $PARENT_DIR/$PROJECT_DIR"
         fi
