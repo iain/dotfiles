af874fa0af5eb126e95628c368596a22c73e4295
iain <iain@iain.nl>
2011-09-11 15:14:00 +0200

    Fix for rvm prompt not updating properly

--- zsh/90_rvm.zsh
+++ zsh/90_rvm.zsh
@@ -2,5 +2,10 @@
 if [ -s ~/.rvm/scripts/rvm ]; then
   source ~/.rvm/scripts/rvm
   __rvm_project_rvmrc
-  RPROMPT="$gray$(~/.rvm/bin/rvm-prompt)$reset"
 fi
+
+function ___rvm_prompt {
+  if [ -s ~/.rvm/bin/rvm-prompt ]; then
+    echo "$gray$(~/.rvm/bin/rvm-prompt)$reset"
+  fi
+}
--- zsh/95_prompt.zsh
+++ zsh/95_prompt.zsh
@@ -1,2 +1,3 @@
 setopt prompt_subst
 PROMPT='$(~/.osx_settings/prompt $?)'
+RPROMPT='$(___rvm_prompt)'
