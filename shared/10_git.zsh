662bb8e5d85dbfbe9e715dd2cb66c026db9d9553
iain <iain@iain.nl>
2012-06-25 21:01:05 +0200

    Git shortcuts and aliases updated

--- shared/10_git.zsh
+++ shared/10_git.zsh
@@ -1,17 +1,37 @@
-alias s='git status --short'
 alias st='git status'
+alias status='git status'
+
 alias co='git checkout'
+alias checkout='git checkout'
+
 alias ci='git commit'
+alias commit='git commit'
+
 alias up='git pull --ff-only'
+
 alias br='git branch'
+alias branch='git branch'
+
 alias lg='git log -p'
+alias ll='git l'
+alias la='git la'
+
 alias aa='git add --all'
+
 alias d='git diff'
 alias df='git diff'
 alias dc='git diff --cached'
+
 alias f='git fetch'
+alias fetch='git fetch'
 alias gf='git fetch && git status'
 
 alias cdb='base=$(git rev-parse --show-cdup) && cd $base'
 
-alias git-scoreboard="git log | grep '^Author' | sort | uniq -ci | sort -r"
+alias push='git push'
+
+unstage() {
+  git reset HEAD -- $*
+  echo
+  git status
+}
