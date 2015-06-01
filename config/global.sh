adc7fef4284fefff1b216aca205c4497d398a765
iain <iain@iain.nl>
2015-06-01 15:20:40 -0400

    Remove disabling -m option in git commit

--- config/global.sh
+++ config/global.sh
@@ -135,16 +135,16 @@ function psr {
   ps aux | grep -v "Pow" | grep "[$FIRST]$REST"
 }
 
-function git {
-  args="$@";
-  if [[ -z $(grep "commit" <<< "$args") ]]; then
-    /usr/local/bin/git "$@"
-  else
-    message=$(echo "$args" | ruby -e 'ARGF.read.split(/\s/).find { |arg| arg =~ /\A-[^-]/ ? arg.include?("m") : arg.include?("--message") } ? exit(1) : exit(0)')
-    if [[ $? == 0 ]]; then
-      /usr/local/bin/git "$@"
-    else
-      echo "Don't use -m."
-    fi
-  fi
-}
+# function git {
+#   args="$@";
+#   if [[ -z $(grep "commit" <<< "$args") ]]; then
+#     /usr/local/bin/git "$@"
+#   else
+#     message=$(echo "$args" | ruby -e 'ARGF.read.split(/\s/).find { |arg| arg =~ /\A-[^-]/ ? arg.include?("m") : arg.include?("--message") } ? exit(1) : exit(0)')
+#     if [[ $? == 0 ]]; then
+#       /usr/local/bin/git "$@"
+#     else
+#       echo "Don't use -m."
+#     fi
+#   fi
+# }
