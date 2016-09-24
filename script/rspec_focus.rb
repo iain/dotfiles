dc0d5f25a2f616effdea1856c9d50b2eda543287
iain <iain@iain.nl>
2016-09-23 23:05:06 -0400

    With script and rspec_focus update

--- script/rspec_focus.rb
+++ script/rspec_focus.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
+  config.reset
   config.formatter = "documentation"
   formatters = config.formatters
   formatters.singleton_class.send(:define_method, :<<) { |*| }
   config.singleton_class.send(:define_method, :formatters) { formatters }
-  # config.before(:suite) { system "clear" }
 end
