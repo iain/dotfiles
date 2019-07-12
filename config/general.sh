0f355d2639edfa8f140e51362cc53e48e85f2e5d
iain <iain@iain.nl>
2019-07-12 17:42:22 -0400

    Delete squashed branches

--- config/general.sh
+++ config/general.sh
@@ -83,6 +83,10 @@ unstage() {
   git status
 }
 
+git-delete-squashed-branches() {
+  git checkout -q master && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base master $branch) && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done
+}
+
 # =============================================================================
 # Ruby config
 # =============================================================================
