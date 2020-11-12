53c832af41ce4edd31db5e6a260bdc8783144433
iain <iain@iain.nl>
2020-11-12 09:49:04 -0500

    prefer binstubs in ruby projects

--- config/ruby.sh
+++ config/ruby.sh
@@ -1,62 +1,30 @@
-# Load a Ruby version manager
-# if [[ -a /usr/local/share/chruby/chruby.sh ]]; then
-#   source /usr/local/share/chruby/chruby.sh
-#   source /usr/local/share/chruby/auto.sh
-# elif which rbenv > /dev/null; then
-#   eval "$(rbenv init - zsh --no-rehash)";
-# elif [ -s ~/.rvm/scripts/rvm ]; then
-#   source ~/.rvm/scripts/rvm
-#   __rvm_project_rvmrc
-# fi
+function __prefer_binstub() {
+  if [ -f "./bin/$1" ]; then
+    ./bin/$@
+  else
+    command $@
+  fi
+}
 
-# Ruby Optimalizations
-# export RUBY_HEAP_MIN_SLOTS=1100000 (obsolete in Ruby 2.1)
-# export RUBY_GC_HEAP_INIT_SLOTS=1100000
-# export RUBY_GC_MALLOC_LIMIT=110000000
-# export RUBY_HEAP_FREE_MIN=20000
-# export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-# export MALLOC_ARENA_MAX=2
+function bundle()   { __prefer_binstub bundle   $@ }
+function cucumber() { __prefer_binstub cucumber $@ }
+function puma()     { __prefer_binstub puma     $@ }
+function rails()    { __prefer_binstub rails    $@ }
+function rake()     { __prefer_binstub rake     $@ }
+function rspec()    { __prefer_binstub rspec    $@ }
+function rubocop()  { __prefer_binstub rubocop  $@ }
+function sidekiq()  { __prefer_binstub sidekiq  $@ }
+function spring()   { __prefer_binstub spring   $@ }
+function yarn()     { __prefer_binstub yarn     $@ }
 
-# Ruby aliases
+# Used by vim-test to run tests in iTerm via AppleScript
+function rspec_focus() {
+  __prefer_binstub rspec --require ~/.dotfiles/script/rspec_focus --order default --color $*
+}
+
 alias r='rails'
 alias b='bundle install'
 alias be='bundle exec'
 alias cu='cucumber'
 alias rr='mkdir -p tmp && touch tmp/restart.txt'
 alias wip='cucumber --profile wip'
-
-# uses spring if available,
-# otherwise bundle exec
-# otherwise real command
-function _bundle_exec() {
-  if [ -f "./bin/spring" ]; then
-    ./bin/spring $*
-  elif [ -f "Gemfile" ]; then
-    bundle exec $*
-  else
-    command $*
-  fi
-}
-
-function rails() {
-  _bundle_exec rails $*
-}
-
-# the following overrides existing ruby commands to hook up into Spring
-# this might not be to your liking.
-
-function rspec() {
-  _bundle_exec rspec $*
-}
-
-function rake() {
-  _bundle_exec rake $*
-}
-
-function cucumber() {
-  _bundle_exec cucumber $*
-}
-
-function rspec_focus() {
-  _bundle_exec rspec --require ~/.dotfiles/script/rspec_focus --order default --color $*
-}
